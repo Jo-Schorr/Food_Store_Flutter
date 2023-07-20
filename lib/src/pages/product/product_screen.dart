a4696121ee08a193f747dc5e1e2c1a8571395b5d
MrSchorr <josueschorr@hotmail.com>
2023-07-19 23:09:14 -0300

    add: info apple

--- lib/src/pages/product/product_screen.dart
+++ lib/src/pages/product/product_screen.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:greengrocer/src/models/item_model.dart';
+import 'package:greengrocer/src/utils_services.dart';
 
 class ProductScreen extends StatelessWidget {
-  const ProductScreen({
+  ProductScreen({
     Key? key,
     required this.item,
   }) : super(key: key);
 
   final ItemModel item;
-
+  final UtilisServices utilisServices = UtilisServices();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,6 +21,7 @@ class ProductScreen extends StatelessWidget {
           ),
           Expanded(
             child: Container(
+              padding: const EdgeInsets.all(32),
               decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: const BorderRadius.vertical(
@@ -30,6 +32,39 @@ class ProductScreen extends StatelessWidget {
                     color: Colors.grey.shade600,
                     offset: const Offset(0, 2),
                   ),
+                ],
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  // Nome -  Quantidade
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Text(
+                          item.itemName,
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          style: const TextStyle(
+                            fontSize: 27,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        height: 30,
+                        width: 70,
+                        color: Colors.red,
+                      ),
+                    ],
+                  ),
+                  // Preco
+                  Text(
+                    utilisServices.priceToCurrency(item.price),
+                  ),
+                  // Descricao
+
+                  //  Botao
                 ],
               ),
             ),
